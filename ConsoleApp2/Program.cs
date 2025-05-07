66a3c11b0d396a0a6b18734cec057cd31462ca3c
VolkovD24 <dfsgdfghcfvd@gmail.com>
2025-05-08 02:17:23 +0300

    «Выполнен ввод значения х.»

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.Write(&quot; Введите значения параметра x = &quot;);
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
         }
     }
 }
